97523695c378e7c0acae130167a4592ffb1d796e
JamesDundon5 <50011074+JamesDundon5@users.noreply.github.com>
2020-01-04 21:33:17 +1100

    Tests

---
+++ Text Replacement.cpp
@@ -0,0 +1,15 @@
+// Libraries
+#include <string>
+#include <iostream>
+
+// Code
+int main() {
+    std::cout << "Found!";
+}
+//std::string str1 = "This is a test.";
+//std::string str2 = "test";
+
+//if(str1.find(str2) != std::string::npos) {
+
+
+//}
