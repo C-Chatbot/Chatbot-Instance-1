4a4991c389fe66867e1236254370a8a9325c8d05
JamesDundon5 <50011074+JamesDundon5@users.noreply.github.com>
2020-01-06 10:13:10 +1100

    Additional Content

--- Main.cpp
+++ Main.cpp
@@ -15,6 +15,7 @@ std::map<std::string, std::string> loadData()
     inFile.open("Training Data.txt");
     while (getline(inFile, line))       // reads text file
     {
+        
         data.push_back(line);
     }
     inFile.close();
@@ -46,8 +47,5 @@ int main()
 {
     std::map<std::string, std::string> training = loadData();
     
-
-
-
     return 0;
 }
